60c0a2e6705bea1df37331455966b586e4554340
Michael Moreno <mmoreno2222@gmail.com>
2020-09-27 13:16:35 -0700

    All view and add options running

--- employeeTracker.js
+++ employeeTracker.js
@@ -30,15 +30,30 @@ inquirer.prompt({
             type: "list",
             name: "menuChoice",
             message: "What would you like to do?",
-            choices: ["View all Employees", "Add Employee"]
+            choices: ["View all Employees", "View all Roles", "View all Departments", "Add Employee", "Add Role", "Add Department", "End Program"]
         }).then(res => {
             switch (res.menuChoice) {
                 case "View all Employees":
                     viewAll();
                     break;
+                case "View all Roles":
+                    viewRoles();
+                    break;
+                case "View all Departments":
+                    viewDepartments();
+                    break;
                 case "Add Employee":
                     addEmployee();
                     break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "End Program":
+                    console.log("Goodbye.");
+                    connection.end();
             };
         });
     }
@@ -56,69 +71,164 @@ inquirer.prompt({
             console.log();
             console.table(res);
             console.log();
+            startMenu();
         })
-        startMenu();
         // connection.end();
     }
-
-    function listRoles() {
-        let query = "SELECT title FROM role"
+    
+    function viewRoles() {
+        let query = "SELECT role.id, role.title, role.salary, department.department_name FROM role "
+        query += "INNER JOIN department ON department.id = role.department_id"
         connection.query(query, (err, res) => {
             if (err) throw err;
-            var rolesArr = []
-            for (var i=0; i < res.length; i++) {
-                choices.push(res[i].title)
-            }
-            return rolesArr
+            console.log();
+            console.table(res);
+            console.log();
+            startMenu();
         })
     }
 
-    function listManagers() {
-        let query = "SELECT * FROM employee WHERE manager_id IS NULL"
+    function viewDepartments() {
+        let query = "SELECT * FROM department"
         connection.query(query, (err, res) => {
             if (err) throw err;
-            let managerArr = [];
-            for (var i=0;i < res.length; i++) {
-                managerArr.push(res[i].first_name + " " + res[i].last_name)
-            }
-            return managerArr
-        });
+            console.log();
+            console.table(res);
+            console.log();
+            startMenu();
+        })
     }
 
     function addEmployee() {
-        var roleArr = listRoles()
-        var managerChoices = listManagers()
-        console.log(managerChoices)
-        connection.end;
-        // inquirer.prompt([
-        //     {
-        //         type: "input",
-        //         name: "firstName",
-        //         message: "Please enter employee's First Name: "
-        //     },
-        //     {
-        //         type: "input",
-        //         name: "lastName",
-        //         message: "Please enter employee's Last Name: "
-        //     },
-        //     {
-        //         type: "input",
-        //         name: "role",
-        //         message: "Please enter employee's Role: "
-        //     },
-        //     {
-        //         type: "list",
-        //         name: "manager",
-        //         message: "Please choose this employee's manager",
-        //         choices: managerChoices
-        //     }
-        // ]).then(result => {
-        //     // let query = "INSERT INTO employee"
-        //     // connection.query(query)
-        //     console.log("Yuh I'll add this in a minute")
-        //     console.log(result)
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "firstName",
+                message: "Please enter employee's First Name:"
+            },
+            {
+                type: "input",
+                name: "lastName",
+                message: "Please enter employee's Last Name:"
+            }
+        ]).then(nameResult => {
+            connection.query("SELECT * FROM role", (err, roleRes) => {
+                if (err) throw err;
+                let rolesArr = []
+                for (var i=0; i < roleRes.length; i++) {
+                    rolesArr.push(roleRes[i].title)
+                }
+                inquirer.prompt({
+                    type: "list",
+                    name: "role",
+                    message: "Please choose this employee's role:",
+                    choices: rolesArr
+                }).then(roleResult => {
+                    for (var i=0; i< roleRes.length; i++) {
+                        if (roleRes[i].title == roleResult.role) {
+                            var newRole = roleRes[i].id
+                        }
+                    }
+                    connection.query("SELECT * FROM employee WHERE manager_id IS NULL", (err, manRes) => {
+                        if (err) throw err;
+                        let managerArr = [];
+                        for (var i=0;i < manRes.length; i++) {
+                            let manName = manRes[i].first_name + " " + manRes[i].last_name
+                            managerArr.push(manName)
+                        }
+                        inquirer.prompt({
+                            type: "list",
+                            name: "manager",
+                            message: "Please choose this employee's manager:",
+                            choices: managerArr
+                        }).then(manResult => {
+                            for (var i=0; i<manRes.length; i++) {
+                                let manName = manRes[i].first_name + " " + manRes[i].last_name
+                                if (manName == manResult.manager) {
+                                    newManager = manRes[i].id
+                                }
+                            }
+                            connection.query("INSERT INTO employee SET ?", 
+                            {
+                                first_name: nameResult.firstName,
+                                last_name: nameResult.lastName,
+                                role_id: newRole,
+                                manager_id: newManager
+                            }, (err) => {
+                                if (err) throw err;
+                                console.log("The employee has been added");
+                                console.log();
+                                startMenu();
+                            }) 
+                        }) 
+                    })
+                }) 
+            }) 
+        }) 
+    } // close function
 
-        //     startMenu();
-        // })
+    function addRole() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "title",
+                message: "Please enter the role you would like to add:"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "Please enter this role's salary:"
+            },
+        ]).then(roleResult => {
+            connection.query("SELECT * FROM department", (err, depRes) => {
+                if (err) throw err;
+                let depArr = [];
+                for (var i=0; i < depRes.length; i++) {
+                    let dep = depRes[i].department_name
+                    depArr.push(dep)
+                }
+                inquirer.prompt({
+                    type: "list",
+                    name: "department",
+                    message: "Please choose this role's department:",
+                    choices: depArr
+                }).then(depResult => {
+                    for (var i=0; i< depRes.length; i++) {
+                        if (depRes[i].department_name == depResult.department) {
+                            var department = depRes[i].id
+                        };
+                    };
+                    connection.query("INSERT INTO role SET ?", 
+                    {
+                        title: roleResult.title,
+                        salary: roleResult.salary,
+                        department_id: department
+                    }, (err) => {
+                        if (err) throw err;
+                        console.log("The new role has been added.");
+                        console.log();
+                        startMenu();
+                    })
+                })
+            })
+        })
+    };
+
+    function addDepartment() {
+        inquirer.prompt({
+            type: "input",
+            name: "department",
+            message: "Please enter the new department:"
+        }).then(result => {
+            connection.query("INSERT INTO department SET ?",
+            {
+                department_name: result.department
+            }, (err) => {
+                if (err) throw err;
+                console.log("The new department has been added.");
+                console.log();
+                startMenu();
+            })
+        })
     }
 })
